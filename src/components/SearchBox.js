f4097f0ea3ffc4b2963a892350810a14a242951c
sophiet12 <57049410+sophiet12@users.noreply.github.com>
2020-02-26 10:28:50 +0100

    first commit

---
+++ src/components/SearchBox.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const SearchBox = ({searchField, searchChange}) => {
+	return (
+		<div className='pa2'>
+			<input
+			  className='pa3 ba b--green br1 bg-lightest-blue'
+			  type='search'
+			  placeholder='search...'
+			  onChange = {searchChange}
+			/>
+		</div>
+	);
+}
+
+export default SearchBox;
